75696886d6e645999dc2671074e94638519f6a71
LEONARDO DE NOVAES VIVI <21276367@CFP127ALUNO.LOCAL>
2021-09-09 15:34:54 -0300

    beta 1.0

--- src/calcArea/CalcArea.java
+++ src/calcArea/CalcArea.java
@@ -8,8 +8,8 @@ public class CalcArea {
 		Scanner leitor = new Scanner(System.in);
 
 		String name;
-		String resposta = "S";
-		int largura, compri, result;
+		String resposta;
+		int largura, compri, result, cont;
 
 		System.out.println("-------------------------------");
 		System.out.println("  Calculo de área de uma casa");
@@ -24,14 +24,37 @@ public class CalcArea {
 		System.out.println("Qual o comprimento?");
 		compri = leitor.nextInt();
 
-		leitor.close();
-
 		result = largura * compri;
 
 		System.out.println("A Área do(a) " + name + " é de " + result + " metros quadrados");
+		cont = result;
 
 		System.out.println("Você deseja calcular mais cômodos(S/N)?");
 		resposta = leitor.next();
 
+		while (resposta.equals("S") || resposta.equals("s")) {
+
+			System.out.println("Qual o nome do cômodo?");
+			name = leitor.next();
+
+			System.out.println("Qual a largura?");
+			largura = leitor.nextInt();
+
+			System.out.println("Qual o comprimento?");
+			compri = leitor.nextInt();
+
+			System.out.println("Você deseja calcular mais cômodos(S/N)?");
+			resposta = leitor.next();
+
+			cont = result + largura * compri;
+		}
+		if (resposta.equals("N") || resposta.equals("n")) {
+			System.out.println("A área total da casa é " + cont + " metros quadrados.");
+		} else {
+			System.out.println("a letra não corresponde");
+		}
+		leitor.close();
+
 	}
+
 }
